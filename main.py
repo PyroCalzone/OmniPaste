89e03d438ef3c0aa939ddea8129941dc3d64188f
PyroCalzone <84557296+PyroCalzone@users.noreply.github.com>
2022-10-10 17:58:33 -0500

    Main.py update
    Paste Feature,
    function skeletons,
    finished a few TODOs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from system_hotkey import SystemHotkey
+import keyboard
 hk = SystemHotkey()
 
 registered = False
@@ -16,31 +17,39 @@ clipboards = {
     "group10": "",
 }
 
-def paste(text): #Paste text, or have the keyboard write it out. TODO: find and integrate a method to write the text without having held modifiers affect the output.
-    print(f"Pasting: {text}")
+def getSelectedText(): #Grab selected text from whatever active window. Might have to modify clipboard history.
+    #TODO: Find a method to do intended functionality.
+    pass
 
+def paste(text): #Paste text, or have the keyboard write it out.
+    keyboard.release("control")
+    keyboard.write(text)
+    keyboard.press("control")
 
-def registerHotkeys(hotKey): #Register Hotkeys for togglable functionality. TODO: Change copy callbacks to grab selected text and save in dictionary
-    hotKey.register(('control', 'alt', '1'), callback=lambda x: print("Copy Group 1"))
+def copy(group): #Copy text
+    clipboards[group] = getSelectedText()
+
+def registerHotkeys(hotKey): #Register Hotkeys for togglable functionality.
+    hotKey.register(('control', 'alt', '1'), callback=lambda x: copy("group1"))
     hotKey.register(('control', '1'), callback=lambda x: paste(clipboards["group1"]))
-    hotKey.register(('control', 'alt', '2'), callback=lambda x: print("Copy Group 2"))
-    hotKey.register(('control', '2'), callback=lambda x: print("Paste Group 2"))
-    hotKey.register(('control', 'alt', '3'), callback=lambda x: print("Copy Group 3"))
-    hotKey.register(('control', '3'), callback=lambda x: print("Paste Group 3"))
-    hotKey.register(('control', 'alt', '4'), callback=lambda x: print("Copy Group 4"))
-    hotKey.register(('control', '4'), callback=lambda x: print("Paste Group 4"))
-    hotKey.register(('control', 'alt', '5'), callback=lambda x: print("Copy Group 5"))
-    hotKey.register(('control', '5'), callback=lambda x: print("Paste Group 5"))
-    hotKey.register(('control', 'alt', '6'), callback=lambda x: print("Copy Group 6"))
-    hotKey.register(('control', '6'), callback=lambda x: print("Paste Group 6"))
-    hotKey.register(('control', 'alt', '7'), callback=lambda x: print("Copy Group 7"))
-    hotKey.register(('control', '7'), callback=lambda x: print("Paste Group 7"))
-    hotKey.register(('control', 'alt', '8'), callback=lambda x: print("Copy Group 8"))
-    hotKey.register(('control', '8'), callback=lambda x: print("Paste Group 8"))
-    hotKey.register(('control', 'alt', '9'), callback=lambda x: print("Copy Group 9"))
-    hotKey.register(('control', '9'), callback=lambda x: print("Paste Group 9"))
-    hotKey.register(('control', 'alt', '0'), callback=lambda x: print("Copy Group 10"))
-    hotKey.register(('control', '0'), callback=lambda x: print("Paste Group 10"))
+    hotKey.register(('control', 'alt', '2'), callback=lambda x: copy("group2"))
+    hotKey.register(('control', '2'), callback=lambda x: paste(clipboards["group2"]))
+    hotKey.register(('control', 'alt', '3'), callback=lambda x: copy("group3"))
+    hotKey.register(('control', '3'), callback=lambda x: paste(clipboards["group3"]))
+    hotKey.register(('control', 'alt', '4'), callback=lambda x: copy("group4"))
+    hotKey.register(('control', '4'), callback=lambda x: paste(clipboards["group4"]))
+    hotKey.register(('control', 'alt', '5'), callback=lambda x: copy("group5"))
+    hotKey.register(('control', '5'), callback=lambda x: paste(clipboards["group5"]))
+    hotKey.register(('control', 'alt', '6'), callback=lambda x: copy("group6"))
+    hotKey.register(('control', '6'), callback=lambda x: paste(clipboards["group6"]))
+    hotKey.register(('control', 'alt', '7'), callback=lambda x: copy("group7"))
+    hotKey.register(('control', '7'), callback=lambda x: paste(clipboards["group7"]))
+    hotKey.register(('control', 'alt', '8'), callback=lambda x: copy("group8"))
+    hotKey.register(('control', '8'), callback=lambda x: paste(clipboards["group8"]))
+    hotKey.register(('control', 'alt', '9'), callback=lambda x: copy("group9"))
+    hotKey.register(('control', '9'), callback=lambda x: paste(clipboards["group9"]))
+    hotKey.register(('control', 'alt', '0'), callback=lambda x: copy("group10"))
+    hotKey.register(('control', '0'), callback=lambda x: paste(clipboards["group10"]))
 
 def unregisterHotkeys(hotKey): # Unregister Hotkeys for togglable functionality.
     hotKey.unregister(('control', 'alt', '1'))
@@ -73,12 +82,13 @@ def registerToggle(): #Toggle hotkeys
         registerHotkeys(hk)
         registered = True
 
-#Todo, add code for finding selected text
+#TODO: add notification that you've switched key-binds
+#TODO: add GUI for identifying which binds holds which pastes 
 
-hk.register(('control', 'grave'), callback=lambda x: registerToggle()) #
-input()
-
-#Ensuring hotkeys are unregistered before exiting, not sure if required. 
-if registered:
+if __name__ == '__main__':
+    hk.register(('control', 'grave'), callback=lambda x: registerToggle()) #
+    input()
+    #Ensuring hotkeys are unregistered before exiting, not sure if required. 
+    if registered:
         unregisterHotkeys(hk)
         registered = False
